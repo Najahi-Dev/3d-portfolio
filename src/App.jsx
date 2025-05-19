76b1a733b9ccb258776e3225599af18f1e58016f
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-05-19 06:09:08 +0530

    feat: Add NavBar, LogoSection, and FeatureCards components to enhance layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./components/LogoSection.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 
 const App = () => {
   return (
     <>
+        <NavBar/>
         <Hero/>
         <ShowcaseSection/>
+        <LogoSection/>
+        <FeatureCards/>
     </>
   )
 }
