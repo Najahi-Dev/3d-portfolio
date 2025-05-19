a04052853bd4b67258bfb933931872637590ad1f
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-05-19 19:17:37 +0530

    Refactor component structure and improve FeatureCards layout.
    Moved `LogoSection` from `components` to `sections` for consistency. Updated `FeatureCards` component to dynamically display cards using `abilities` data, enhancing layout and styling. This improves maintainability and visual appearance.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
-import LogoSection from "./components/LogoSection.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 
 const App = () => {
